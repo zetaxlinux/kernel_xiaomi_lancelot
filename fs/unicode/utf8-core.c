9707ab81aa60a28309d82ee68cf5c9b5bd89a8bb
khaeruirgi <83441763+khaeruirgi@users.noreply.github.com>
2021-09-23 19:25:16 +0700

    My Cat is Abu or Grey

--- fs/unicode/utf8-core.c
+++ fs/unicode/utf8-core.c
@@ -138,11 +138,7 @@ int utf8_casefold_hash(const struct unicode_map *um, const void *salt,
 
 	while ((c = utf8byte(&cur))) {
 		if (c < 0)
-<<<<<<< HEAD
-			return c;
-=======
 			return -EINVAL;
->>>>>>> e367cc29397c5dc5d2c63a472287d1eeaf14e0b5
 		hash = partial_name_hash((unsigned char)c, hash);
 	}
 	str->hash = end_name_hash(hash);
