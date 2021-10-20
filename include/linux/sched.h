23c50bf8e5ff1713ff443846bec3e37e6b2f08ce
khaeruirgi <83441763+khaeruirgi@users.noreply.github.com>
2021-10-20 18:38:55 +0700

    sched.h : fix compiling error
    This is fix compiling error' after add slmk

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1387,7 +1387,7 @@ struct task_struct {
 	unsigned short inherit_cnt:14;
 	short nice_backup;
 	atomic_t inherit_types;
-
+#endif
 #ifdef CONFIG_ANDROID_SIMPLE_LMK
 	struct task_struct		*simple_lmk_next;
 #endif
